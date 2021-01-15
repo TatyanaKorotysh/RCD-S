d0f80aa8c88bbf7a2ff9b18072fe54bf401d7db5
Tatyana <Tatyana.Kor@tut.by>
2021-01-15 16:32:07 +0300

    Странеца устройств (не обновляется UI после выбора в выпадающем списке)

--- rcd_s/lib/devices.dart
+++ rcd_s/lib/devices.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Devices extends StatefulWidget {
+  Devices({Key key}) : super(key: key);
+
   @override
   _DevicesState createState() => _DevicesState();
 }
@@ -24,22 +26,115 @@ class _DevicesState extends State<Devices> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Устройства'),
-        actions: [
-          IconButton(icon: Icon(Icons.dehaze_rounded), onPressed: _openMenu),
-        ],
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      body: _body(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addDevice,
-        child: Icon(Icons.add),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Устройства'),
+          actions: [
+            IconButton(icon: Icon(Icons.dehaze_rounded), onPressed: _openMenu),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 60.0,
+                width: MediaQuery.of(context).size.width,
+                child: _block(),
+              ),
+              Expanded(
+                child: _grid(),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _addDevice,
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
 
-  Widget _body() {
+  Widget _block() {
+    String _sortValue = 'One';
+    String _groupValue = 'One';
+
+    return Row(
+      children: <Widget>[
+        Expanded(
+          flex: 5, //50%
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Сортировать по '),
+              DropdownButton<String>(
+                value: _sortValue,
+                icon: Icon(Icons.arrow_downward),
+                iconSize: 24,
+                elevation: 16,
+                style: TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.deepPurpleAccent,
+                ),
+                onChanged: (String newValue) {
+                  setState(() {
+                    _sortValue = newValue;
+                  });
+                },
+                items: <String>['One', 'Two', 'Three', 'Four']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 5, //50%
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Группировать по '),
+              DropdownButton<String>(
+                value: _groupValue,
+                icon: Icon(Icons.arrow_downward),
+                iconSize: 24,
+                elevation: 16,
+                style: TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.deepPurpleAccent,
+                ),
+                onChanged: (String newValue) {
+                  setState(() {
+                    _groupValue = newValue;
+                  });
+                },
+                items: <String>['One', 'Two', 'Three', 'Four']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _grid() {
     final orientation = MediaQuery.of(context).orientation;
     return GridView.builder(
       itemCount: data.length,
@@ -47,10 +142,18 @@ class _DevicesState extends State<Devices> {
           crossAxisCount: (orientation == Orientation.portrait) ? 2 : 3),
       itemBuilder: (BuildContext context, int index) {
         return new Card(
-          child: new GridTile(
-            child: Text(data[index]),
-            footer: Text('Комната'),
-          ),
+          child: InkWell(
+              splashColor: Colors.indigo.withAlpha(30),
+              onTap: () {},
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Icon(Icons.alarm),
+                  new Text(data[index]),
+                  new Text('Комната'),
+                ],
+              )),
         );
       },
     );
